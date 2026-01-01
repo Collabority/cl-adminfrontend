105b227c5bf393edefa44717614aaaaac5e5a89d
yadnesh ranshevare <yranshevare2005@gmail.com>
2026-01-01 13:30:53 +0530

    comment out the use login hool from src/hooks/useLogin.jsx

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import { useDispatch } from "react-redux";
-// import { loginSuccess } from "@/store/authSlice";
-import instance from "../lib/axios";
-import { login } from "../redux/authSlice";
+// import { useDispatch } from "react-redux";
+// // import { loginSuccess } from "@/store/authSlice";
+// import instance from "../lib/axios";
+// import { login } from "../redux/authSlice";
 
-function useLogin() {
-  const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch();
+// function useLogin() {
+//   const [loading, setLoading] = useState(false);
+//   const dispatch = useDispatch();
 
-  const loginUser = async (data) => {
-    setLoading(true);
-    try {
-      const response = await instance.post("/admin/login", data);
+//   const loginUser = async (data) => {
+//     setLoading(true);
+//     try {
+//       const response = await instance.post("/admin/login", data);
 
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(response.data?.message || "Registration failed.");
-      }
+//       if (response.status < 200 || response.status >= 300) {
+//         throw new Error(response.data?.message || "Registration failed.");
+//       }
 
-      dispatch(login({ admin: response.data.data }));
-      console.log("Successfully registered. Logging you in...");
-    } catch (error) {
-      const message =
-        error?.response?.data?.message ||
-        error?.message ||
-        "Signup failed. Try again.";
+//       dispatch(login({ admin: response.data.data }));
+//       console.log("Successfully registered. Logging you in...");
+//     } catch (error) {
+//       const message =
+//         error?.response?.data?.message ||
+//         error?.message ||
+//         "Signup failed. Try again.";
 
-      console.error("Login error:", message);
-      //   toast.error(message);
-    } finally {
-      setLoading(false);
-    }
-  };
-  return { loginUser, loading };
-}
+//       console.error("Login error:", message);
+//       //   toast.error(message);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+//   return { loginUser, loading };
+// }
 
-export { useLogin };
+// export { useLogin };
